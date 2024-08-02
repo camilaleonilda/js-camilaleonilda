721e67e96006af1d040200a572a8816a223dc5c0
camila <camila.pilati@escola.pr.gov.br>
2024-08-02 13:43:16 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,26 @@
+function setup() {
+    createCanvas(400, 400);
+    background ("violet");
+  }
+  
+  function draw() {
+    circle(30,20,20);
+    fill("cyan");
+    stroke("blue");
+    if(mouseIsPressed);{
+    circle(mouseX,mouseY,20,20);}
+  }
+  6
+  function draw() {
+  7
+    circle(30,20,20);
+  8
+    fill("cyan");
+  9
+    stroke("blue");
+  10
+    if(mouseIsPressed);{
+  11
+    circle(mouseX,mouseY,20,20);}
+  12
+  }
